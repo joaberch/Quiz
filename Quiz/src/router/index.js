e76eac9fdab6210ad98e089d6a87ccf2d0cff899
cycoborchp <joachimsimeongabriel.berchel@eduvaud.ch>
2024-05-15 17:17:58 +0200

    create the route

--- Quiz/src/router/index.js
+++ Quiz/src/router/index.js
@@ -2,6 +2,7 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Home from '../views/homeMenu.vue';
 import PeriodicTable from '../views/periodicTable/periodictable.vue';
+import SystemeSolaire from '../views/systemeSolaire/systemeSolaire.vue';
 
 const routes = [
   {
@@ -13,6 +14,51 @@ const routes = [
     path: '/periodicTable',
     name: 'periodicTable',
     component: PeriodicTable,
+  },
+  {
+    path: '/allemand',
+    name: 'allemand',
+    component: allemand,
+  },
+  {
+    path: '/anglais',
+    name: 'anglais',
+    component: anglais,
+  },
+  {
+    path: '/capital',
+    name: 'capital',
+    component: capital,
+  },
+  {
+    path: '/constellation',
+    name: 'constellation',
+    component: constellation,
+  },
+  {
+    path: '/drapeau',
+    name: 'drapeau',
+    component: drapeau,
+  },
+  {
+    path: '/especeAnimal',
+    name: 'especeAnimal',
+    component: especeAnimal,
+  },
+  {
+    path: '/multiplication',
+    name: 'multiplication',
+    component: multiplication,
+  },
+  {
+    path: '/pi',
+    name: 'pi',
+    component: pi,
+  },
+  {
+    path: '/systemeSolaire',
+    name: 'systemeSolaire',
+    component: systemeSolaire,
   }
 ];
 
