daf4ab230ac0f143f2b09aa0857242e48e1e3e7f
cycoborchp <joachimsimeongabriel.berchel@eduvaud.ch>
2024-05-22 20:54:02 +0200

    create the periodic table view

---
+++ Quiz/src/components/periodicTable/element.vue
@@ -0,0 +1,38 @@
+<script setup>
+defineProps({
+    elementData: Object
+})
+</script>
+<template>
+    <div class="square" :class="elementData.category">
+        <p id="number">{{ elementData.number }}</p>
+        <p id="symbol">{{ elementData.symbol }}</p>
+        <p id="name">{{ elementData.name }}</p>
+        <p id="atomic_mass">{{ elementData.atomic_mass }}</p>
+    </div>
+</template>
+<style scoped>
+.square {
+    display: flex;
+    flex-direction: column;
+    padding: 5px 5px;
+    margin: 10px;
+    border: black 1px solid;
+    border-radius: 10px;
+}
+p{
+    margin: 0
+}
+#number {
+    font-size: 10px;
+}
+#symbol {
+    font-size: 12px;
+}
+#name {
+    font-size: 8px;
+}
+#atomic_mass {
+    font-size: 8px;
+}
+</style>
--- Quiz/src/views/periodicTable/periodictable.vue
+++ Quiz/src/views/periodicTable/periodictable.vue
@@ -1,9 +1,20 @@
 <script setup>
-
+import Element from '../../components/periodicTable/element.vue';
+import data from '../../utils/periodicTableData';
 </script>
 <template>
-    
+    <h1>Tableau périodique</h1>
+    <div class="table">
+        <Element v-for="d in data" :key="d.number" :elementData="d">{{ d }}</element>
+    </div>
 </template>
 <style scoped>
-
+.table{
+    display: grid;
+    grid-template: repeat(10, 1fr) / repeat(18, 1fr);
+}
+h1 {
+    display: flex;
+    justify-content: center;
+}
 </style>
