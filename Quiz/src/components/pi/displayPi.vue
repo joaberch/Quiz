4c9f92c9ee8de82101a4e9b5e6e7567c236133b1
cycoborchp <joachimsimeongabriel.berchel@eduvaud.ch>
2024-07-27 02:29:10 +0200

    Feat: Add the pi page

---
+++ Quiz/src/components/pi/displayPi.vue
@@ -0,0 +1,37 @@
+<template>
+    <div id="container">
+        <h1>Les chiffres de π :</h1>
+        <label for="digit-input">Nombre de chiffres à afficher :</label>
+        <input id="digit-input" type="number" v-model.number="digitsChosen" min="1" :max="piLength"/>
+        <div id="pi-container">
+            {{ pi.pi.slice(0, digitsChosen+2) }}
+        </div>
+    </div>
+</template>
+<script setup>
+import pi from '../../utils/pi.json'
+import { ref, computed } from 'vue';
+
+let digitsChosen = ref(100)
+let piLength = pi.pi.length
+
+//TODO : space caracters
+</script>
+<style scoped>
+#pi-container {
+    display: flex;
+    flex-wrap: wrap;
+    width: 80%;
+    word-break: break-all;
+}
+#container {
+    margin-top: 80px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+h1 {
+    display: flex;
+    justify-content: center;
+}
+</style>
--- Quiz/src/views/pi/pi.vue
+++ Quiz/src/views/pi/pi.vue
@@ -1,9 +1,8 @@
 <script setup>
-
+import displayPi from '../../components/pi/displayPi.vue';
 </script>
 <template>
-
+<displayPi></displayPi>
 </template>
 <style scoped>
-
 </style>
